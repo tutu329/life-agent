9161faa8902f7e586358833c3ec5462188f9d03b
tutu329 <jack.seaver@163.com>
2023-09-27 17:04:41 +0800

    13900k-server

--- tools/audio_stt.py
+++ tools/audio_stt.py
@@ -5,29 +5,59 @@
 import whisper
 import os
 
-# class audio_stt():
-#     def __int__(self):
-#         pass
+def stt(in_mp3_filename, in_model='base'):
+    # model = whisper.load_model("medium")
+    # model = whisper.load_model("small")
+    # model = whisper.load_model("base")
+    model = whisper.load_model(in_model)
 
-# model = whisper.load_model("medium")
-# model = whisper.load_model("small")
-model = whisper.load_model("base")
+    # load audio and pad/trim it to fit 30 seconds
+    # print('当前文件夹: ', os.getcwd())
+    audio = whisper.load_audio(in_mp3_filename)
+    # audio = whisper.load_audio("output.mp3")
+    audio = whisper.pad_or_trim(audio)
 
-# load audio and pad/trim it to fit 30 seconds
-print('当前文件夹: ', os.getcwd())
-audio = whisper.load_audio("output.mp3")
-audio = whisper.pad_or_trim(audio)
+    # make log-Mel spectrogram and move to the same device as the model
+    mel = whisper.log_mel_spectrogram(audio).to(model.device)
 
-# make log-Mel spectrogram and move to the same device as the model
-mel = whisper.log_mel_spectrogram(audio).to(model.device)
+    # detect the spoken language
+    _, probs = model.detect_language(mel)
+    # print(f"Detected language: {max(probs, key=probs.get)}")
 
-# detect the spoken language
-_, probs = model.detect_language(mel)
-print(f"Detected language: {max(probs, key=probs.get)}")
+    # decode the audio
+    options = whisper.DecodingOptions()
+    result = whisper.decode(model, mel, options)
 
-# decode the audio
-options = whisper.DecodingOptions()
-result = whisper.decode(model, mel, options)
+    # print the recognized text
+    # print(result.text)
+    return result.text
 
-# print the recognized text
-print(result.text)
+def main():
+    res = stt('output.mp3')
+    print('语音所包含文字为：', res)
+
+if __name__ == "__main__":
+    main()
+
+# # model = whisper.load_model("medium")
+# # model = whisper.load_model("small")
+# model = whisper.load_model("base")
+#
+# # load audio and pad/trim it to fit 30 seconds
+# print('当前文件夹: ', os.getcwd())
+# audio = whisper.load_audio("output.mp3")
+# audio = whisper.pad_or_trim(audio)
+#
+# # make log-Mel spectrogram and move to the same device as the model
+# mel = whisper.log_mel_spectrogram(audio).to(model.device)
+#
+# # detect the spoken language
+# _, probs = model.detect_language(mel)
+# print(f"Detected language: {max(probs, key=probs.get)}")
+#
+# # decode the audio
+# options = whisper.DecodingOptions()
+# result = whisper.decode(model, mel, options)
+#
+# # print the recognized text
+# print(result.text)
