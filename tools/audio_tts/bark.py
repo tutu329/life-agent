9f64b964cc57e6b44b4ac8c71b12e4bce39c8dc4
tutu329 <jack.seaver@163.com>
2023-10-05 14:12:24 +0800

    13900k-server

--- tools/audio_tts/bark.py
+++ tools/audio_tts/bark.py
@@ -1,24 +1,3 @@
-# from bark import SAMPLE_RATE, generate_audio, preload_models
-# from scipy.io.wavfile import write as write_wav
-# from IPython.display import Audio
-#
-# # download and load all models
-# preload_models()
-#
-# # generate audio from text
-# text_prompt = """
-#      Hello, my name is Suno. And, uh — and I like pizza. [laughs]
-#      But I also have other interests such as playing tic tac toe.
-# """
-# audio_array = generate_audio(text_prompt)
-#
-# # save audio to disk
-# write_wav("bark_generation.wav", SAMPLE_RATE, audio_array)
-#
-# # play text in notebook
-# Audio(audio_array, rate=SAMPLE_RATE)
-
-
 from transformers import AutoProcessor, BarkModel
 import scipy
 import time
@@ -31,8 +10,6 @@ print('====================2========================')
 model = BarkModel.from_pretrained("D:/models/bark-small")
 model.to('cuda')
 print('====================3========================')
-
-
 
 # 装饰器
 def get_run_time(func):
@@ -84,8 +61,6 @@ def t2s():
     print('====================5========================')
     audio_array = audio_array.cpu().numpy().squeeze()
     print('====================6========================')
-
-
 
     sample_rate = model.generation_config.sample_rate
     scipy.io.wavfile.write("bark_out.wav", rate=sample_rate, data=audio_array)
