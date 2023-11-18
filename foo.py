823ab64890d52362d6acefa7f87c4be4c2ab153c
tutu329 <jack.seaver@163.com>
2023-11-18 21:25:31 +0800

    13900k-server

--- foo.py
+++ foo.py
@@ -36,6 +36,46 @@ class Session_Data():
     ip: str = ''
     chat_history: List[One_Chat] = field(default_factory=list)
 
+def p2p_speed():
+    import torch
+    import numpy as np
+
+    device = torch.device("cuda")
+    print(f'torch version: {torch.__version__}')
+    print(f'torch.cuda.is_available: {torch.cuda.is_available()}')
+    print(f'device: {device}')
+    
+    print(f'torch.cuda.device(0): {torch.cuda.device(0)}')
+    print(f'torch.cuda.device(1): {torch.cuda.device(1)}')
+
+    n_gpus = 2
+    data_size = 1024 * 1024 * 1024  # 1 GB
+
+    speed_matrix = np.zeros((n_gpus, n_gpus))
+
+    for i in range(n_gpus):
+        for j in range(i + 1, n_gpus):
+            print(f"Testing communication between GPU {i} and GPU {j}...")
+            with torch.cuda.device(i):
+                data = torch.randn(data_size, device=device)
+                torch.cuda.synchronize()
+            with torch.cuda.device(j):
+                result = torch.randn(data_size, device=device)
+                torch.cuda.synchronize()
+            with torch.cuda.device(i):
+                start = torch.cuda.Event(enable_timing=True)
+                end = torch.cuda.Event(enable_timing=True)
+                start.record()
+                result.copy_(data)
+                end.record()
+                torch.cuda.synchronize()
+                elapsed_time_ms = start.elapsed_time(end)
+            transfer_rate = data_size / elapsed_time_ms * 1000 * 8 / 1e9
+            speed_matrix[i][j] = transfer_rate
+            speed_matrix[j][i] = transfer_rate
+
+    print(speed_matrix)
+
 def main2():
     a = Session_Data()
     print(a)
@@ -54,4 +94,5 @@ def main1():
 
 
 if __name__ == '__main__':
-    main1()
+    # main1()
+    p2p_speed()
---
+++ utils/nvlink_p2p_speed.py
@@ -0,0 +1,50 @@
+import torch
+import numpy as np
+
+def nvlink_p2p_speed(gpu_num=2):
+    print(f'torch version: {torch.__version__}')
+    print(f'torch.cuda.is_available: {torch.cuda.is_available()}')
+    print(f'torch.device: {torch.device("cuda")}')
+
+    device = torch.device("cuda")
+    n_gpus = gpu_num
+    data_size = 1024 * 1024 * 1024  # 1 GB
+
+    speed_matrix = np.zeros((n_gpus, n_gpus))
+
+    for i in range(n_gpus):
+        for j in range(i + 1, n_gpus):
+            print(f"测试 GPU {i} 和 GPU {j} 之间的通信速度...")
+            with torch.cuda.device(i):
+                data = torch.randn(data_size, device=device)
+                torch.cuda.synchronize()
+            with torch.cuda.device(j):
+                result = torch.randn(data_size, device=device)
+                torch.cuda.synchronize()
+            with torch.cuda.device(i):
+                start = torch.cuda.Event(enable_timing=True)
+                end = torch.cuda.Event(enable_timing=True)
+                start.record()
+                result.copy_(data)
+                end.record()
+                torch.cuda.synchronize()
+                elapsed_time_ms = start.elapsed_time(end)
+            transfer_rate = data_size / elapsed_time_ms * 1000 * 8 / 1e9
+            speed_matrix[i][j] = transfer_rate
+            speed_matrix[j][i] = transfer_rate
+
+    print(f'{"":>5}', end='')
+    for i in range(n_gpus):
+        gpu_string = f'GPU{i:>2}'
+        print(f'{gpu_string:>13}', end='')   # >为右对齐，<为左对齐，^为居中对齐
+    print()
+
+    for i in range(n_gpus):
+        gpu_string = f'GPU{i:>2}'
+        print(f'{gpu_string}', end='')
+        for j in range(n_gpus):
+            print(f'{speed_matrix[i][j]:>8.2f} GB/s', end='')
+        print()
+
+if __name__ == '__main__':
+    nvlink_p2p_speed(gpu_num=2)
