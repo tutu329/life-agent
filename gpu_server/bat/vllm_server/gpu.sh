9399a9219c33eefa779c9b0e2e9cce5dc152a18b
tutu <jack.seaver@163.com>
2024-03-10 10:42:16 +0800

    gpu-server

---
+++ gpu_server/bat/vllm_server/gpu.sh
@@ -0,0 +1,3 @@
+sudo nvidia-smi -pm 1 # 设置显卡持久模式
+sudo nvidia-smi -lgc 1400 # 固定显卡频率
+sudo nvidia-smi -pl 200 # 设置显卡最大功率
---
+++ gpu_server/bat/vllm_server/gpu_full_backup.sh
@@ -0,0 +1,6 @@
+sudo nvidia-smi -pm 1
+sudo nvidia-smi -lgc 1400
+sudo nvidia-smi -lmc 6500
+sudo nvidia-smi -gtt 65
+sudo nvidia-smi -cc 1
+sudo nvidia-smi -pl 200
