f58c086164f8a097ac3a78c48c8efff56c8b51cd
tutu329 <jack.seaver@163.com>
2024-05-28 17:57:00 +0800

    13900k-server

--- gpu_server/redis_monitor.py
+++ gpu_server/redis_monitor.py
@@ -1,3 +1,4 @@
+import time
 import streamlit as st
 
 from redis_client import Redis_Client, from_dict
@@ -5,7 +6,7 @@ from tools.llm.api_client import LLM_Client_Status
 
 st.set_page_config(
     initial_sidebar_state="collapsed",
-    page_title="Life-Agent",
+    page_title="Redis Monitor",
     layout="wide",
 )
 
@@ -16,14 +17,38 @@ def redis_init():
 
 r = redis_init()
 
-
-
 def streamlit_refresh_loop():
-    status = r.get_dict('LLM_Client status')
-    data = from_dict(LLM_Client_Status, status)
-    st.markdown(status)
-    st.markdown(data)
-    # st.rerun()
+    # 创建标签页
+    tab1, tab2 = st.tabs(["LLM", "other"])
+
+    # 主循环，每秒刷新一次数据
+    while True:
+        # ----------------------------------tab1----------------------------------------
+        status = r.get_dict('LLM_Client status')
+        data = from_dict(LLM_Client_Status, status)
+
+        tab1.markdown(f'【问题】  {data.question}')
+        tab1.markdown(f'【uuid】 {data.uuid}')
+        tab1.markdown(f'【system prompt】 {data.system_prompt}')
+        tab1.markdown(f'【role prompt】 {data.role_prompt}')
+        tab1.markdown(f'【temperature】 {data.temperature}')
+        tab1.markdown(f'【stops】 {data.stops}')
+        tab1.markdown(f'【max new tokens】 {data.max_new_tokens}')
+        tab1.markdown(f'【上一次回复】 {data.last_response}')
+        tab1.markdown(f'【对话历史】')
+        for chat in data.history_list:
+            tab1.markdown(f"{'&nbsp;'*16}【{chat['role']}】 {chat['content']}")
+
+
+
+        # ----------------------------------tab2----------------------------------------
+        tab2.markdown('dd')
+        tab2.markdown('hi')
+
+        # ----------------------------------刷新----------------------------------------
+        time.sleep(1)
+        st.rerun()
+        # st.experimental_rerun()
 
 if __name__ == "__main__":
     streamlit_refresh_loop()
