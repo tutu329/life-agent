141249f9f9e3fc62c9d69f72e4c116972618a7f3
tutu329 <jack.seaver@163.com>
2024-06-19 16:27:49 +0800

    13900k-server

--- gpu_server/redis_task.py
+++ gpu_server/redis_task.py
@@ -1,4 +1,31 @@
+from utils.task import Task_Base, Status
 from redis_client import Redis_Client
+
+import uuid,time
+
+class Redis_Task_Server(Task_Base):
+    def __init__(self):
+        super().__init__()
+
+    def init(self,
+             in_callback_func,
+             *in_callback_func_args,
+             in_name='redis_task_'+str(uuid.uuid4()),
+             in_timeout=None,    # timeout秒之后，设置cancel标识
+             in_streamlit=False,
+             **in_callback_func_kwargs
+            ):
+        super().init(
+             in_callback_func,
+             *in_callback_func_args,
+             in_name=in_name,
+             in_timeout=in_timeout,
+             in_streamlit=in_streamlit,
+             **in_callback_func_kwargs
+        )
+
+    def start(self):
+        super().start()
 
 def main():
     client = Redis_Client(host='localhost', port=6379)  # win-server
@@ -23,5 +50,21 @@ def main():
     print(f'last2: {last2}')
     print(f'inout_list2: "{inout_list2}')
 
+def Example_Callback(out_task_info, arg1):
+    i=arg1
+    while True:
+        i += 1
+        if out_task_info['status']==Status.Cancelling:
+             print(f"{out_task_info['task_name']} cancelled")
+             break
+        print(i)
+        time.sleep(1)
+
+def main1():
+    t = Redis_Task_Server()
+    t.init(Example_Callback, in_name='task1', in_timeout=3, arg1=100)
+    t.start()
+
 if __name__ == "__main__":
-    main()
+    main1()
+    # main()
