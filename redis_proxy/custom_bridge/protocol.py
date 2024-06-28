d54cdf5fd1c3f71ec59ee449bec525d7e03fbe6a
tutu329 <jack.seaver@163.com>
2024-06-28 11:06:26 +0800

    13900k-server

--- redis_proxy/custom_bridge/protocol.py
+++ redis_proxy/custom_bridge/protocol.py
@@ -1,10 +1,29 @@
+from enum import Enum, unique
+
+from config import dred, dgreen
+
+@unique
+class Redis_Bridge_Type(Enum):
+    TRANSLATE = 'TRANSLATE'
+
 def server_invoking_bridge():
     pass
 
-def server_add_bridge(inout_client_data, bridge_type):
+def server_add_bridge(inout_client_data, bridge_type:str):
+    # 读取bridge_system
+    if 'bridge_system' in inout_client_data:
+        client_bridge_system = inout_client_data['bridge_system']
+    else:
+        inout_client_data['bridge_system'] = {}
+        client_bridge_system = inout_client_data['bridge_system']
+
+    # 读取client现有bridge数量
+    client_bridge_num = len(client_bridge_system)
+    client_bridge_num += 1
+
     # 由于某一类command对应n个bridge，因此bridge不是某个task或者某个task下的某个command的一部分
     # 因此bridge_system是client的一部分
-    bridge_data = {
+    client_bridge_data = {
         'bridge_type':bridge_type,
         'obj': None,
         'thread': None,
@@ -28,12 +47,7 @@ def server_add_bridge(inout_client_data, bridge_type):
 
     # 启动bridge的thread
 
-    # 读取bridge_system
-    if 'bridge_system' in inout_client_data:
-        bridge_system = inout_client_data['bridge_system']
-    else:
-        inout_client_data['bridge_system'] = {}
-        bridge_system = inout_client_data['bridge_system']
+
     # 写入bridge_data
-    bridge_system['bridge_type'] = bridge_data
+    client_bridge_system['bridge_type'] = client_bridge_data
 
