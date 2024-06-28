f4241aff4a93f6648c7d8413a5637de4d8a31127
tutu329 <jack.seaver@163.com>
2024-06-28 10:41:08 +0800

    13900k-server

--- redis_proxy/custom_bridge/protocol.py
+++ redis_proxy/custom_bridge/protocol.py
@@ -8,7 +8,22 @@ def server_add_bridge(inout_client_data, bridge_type):
         'bridge_type':bridge_type,
         'obj': None,
         'thread': None,
+
+        # key的桥接（bridge的核心任务：将原有task中的redis key转为桥接后的key）
+        # 核心流程是：具体某个command执行时，其输入输出的key将被bridge到新的key中（且command流程不需自知）
+        'task_status_bridged_key': None,    # f'Task_{task_id}_Status',
+        'task_result_bridged_key': None,    # f'Task_{task_id}_Result',
     }
+
+    # 创建并启动bridge的轮询任务
+    # bridge_data['obj'] =
+    # bridge_data['thread'] =
+
+    # bridge的轮询任务
+    def bridge_polling():
+        pass
+
+    # 启动bridge的thread
 
     # 读取bridge_system
     if 'bridge_system' in inout_client_data:
