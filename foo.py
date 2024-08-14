8ff5a5bf76cdf8332bd8e5f023f69068c5ecd41f
tutu329 <jack.seaver@163.com>
2024-08-14 14:57:47 +0800

    13900k-server

--- foo.py
+++ foo.py
@@ -2,13 +2,13 @@ from openai import OpenAI
 
 # 设置api-key和LLM的地址
 oai = OpenAI(
-    # api_key='empty',
-    # base_url='http://172.27.67.106:8001/v1',
+    api_key='empty',
+    base_url='http://172.27.67.106:8001/v1',
     # base_url='http://127.0.0.1:8001/v1',
 
     # base_url='http://localhost:8001/v1',
-    api_key='sk-c1d34a4f21e3413487bb4b2806f6c4b8',
-    base_url='https://api.deepseek.com/v1',
+    # api_key='sk-c1d34a4f21e3413487bb4b2806f6c4b8',
+    # base_url='https://api.deepseek.com/v1',
 )
 
 # 流式输出的调用
@@ -26,9 +26,9 @@ def main():
         messages = [{'role': 'system','content': 'You are a helpful assistant.'},{'role': 'user','content': '你是谁？'}]
 
         # 向LLM发送messages
-        # mid=model_id
+        mid=model_id
         # mid='deepseek-chat'
-        mid='deepseek-coder'
+        # mid='deepseek-coder'
         gen = oai.chat.completions.create(model=mid,temperature=0.7,messages=messages,stream=True,max_tokens=1024,)
 
         # 流式输出LLM的回复
