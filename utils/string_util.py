ebc10353141195c862b0e18e6d935b58b6594142
tutu <jack.seaver@163.com>
2024-09-26 11:47:49 +0800

    macbookpro-15

--- utils/string_util.py
+++ utils/string_util.py
@@ -1,11 +1,11 @@
 import re
 
 # str1和str2的交集，且该交集起始和str2起始一致
-def _str_has_partial_stop(str, stop):
+def _str_has_partial_substr(str, substr):
     same_str = ''
     chunk = ''
     # 找到str1和str2交集
-    for ch in stop:
+    for ch in substr:
         chunk += ch
         if chunk in str:
             same_str = chunk
@@ -16,16 +16,16 @@ def _str_has_partial_stop(str, stop):
             break
 
 
-    # 判断partial stop和str1的交集是否为str1尾部，或者完整stop是否in str1
+    # 判断partial substr和str1的交集是否为str1尾部，或者完整substr是否in str1
     # print(f'same:"{same_str}"')
     if str.endswith(same_str):
         return same_str
-    elif stop in str:
-        str = str.split(stop)
+    elif substr in str:
+        str = str.split(substr)
         if len(str)>0:
             # 取第一个stop后面所有
             str.pop(0)
-            return stop+ ''.join(str)
+            return substr+ ''.join(str)
         else:
             # 原来就没有stop
             str = str[0]
@@ -36,26 +36,26 @@ def _str_has_partial_stop(str, stop):
 
 
 # "aaaaaaa<stop"和"<stop>"的交集是否为<开始
-def _str_remove_partial_stop(str, stop):
-    partial_stop = _str_has_partial_stop(str, stop)
+def _str_remove_partial_substring(str, substr):
+    partial_substr = _str_has_partial_substr(str, substr)
     # print(f'partial_stop: "{partial_stop}"')
-    if not partial_stop:
+    if not partial_substr:
         return str
-    str = str.split(partial_stop)
+    str = str.split(partial_substr)
     # print(str)
     str.pop()
     # print(str)
-    str = partial_stop.join(str)
+    str = partial_substr.join(str)
     # print(str)
 
     return str
 
-def str_remove_partial_substring(str, stops):
+def str_remove_partial_substring(str, substrings):
     str1 = str
     # print(f'str: "{str}"')
     min_str = str1
-    for stop in stops:
-        str1 = _str_remove_partial_stop(str, stop)
+    for substr in substrings:
+        str1 = _str_remove_partial_substring(str, substr)
         # print(f'str: "{str}", stop: "{stop}"')
         # print(f'mmm: "{min_str}", stop: "{stop}"')
         if len(min_str) > len(str1):
