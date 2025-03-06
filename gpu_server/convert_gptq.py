56667047d4fd03e17a2dcb5a369171632594eaac
tutu329 <jack.seaver@163.com>
2025-03-06 17:32:20 +0800

    13900k-server

--- gpu_server/convert_gptq.py
+++ gpu_server/convert_gptq.py
@@ -1,114 +1,61 @@
-from transformers import AutoTokenizer, TextGenerationPipeline
-from auto_gptq import AutoGPTQForCausalLM, BaseQuantizeConfig
-import logging
-
-
-# 1、视情况将原有模型文件夹中的modeling_qwen.py修改为下述代码（适用于qwen-14b-chat）
-# =====================把modeling_qwen.py中的apply_rotary_pos_emb（）改为==================================================
-# def apply_rotary_pos_emb(t, freqs):
-#     cos, sin = freqs
-#     cos = cos.to(t.device)
-#     sin = sin.to(t.device)
-#     if apply_rotary_emb_func is not None and t.is_cuda:
-#         t_ = t.float()
-#         cos = cos.squeeze(0).squeeze(1)[:, : cos.shape[-1] // 2]
-#         sin = sin.squeeze(0).squeeze(1)[:, : sin.shape[-1] // 2]
-#         output = apply_rotary_emb_func(t_, cos, sin).type_as(t)
-#         return output
-#     else:
-#         rot_dim = freqs[0].shape[-1]
-#         cos, sin = freqs
-#         cos = cos.to(t.device)
-#         sin = sin.to(t.device)
-#         t_, t_pass_ = t[..., :rot_dim], t[..., rot_dim:]
-#         t_ = t_.float()
-#         t_pass_ = t_pass_.float()
-#         t_ = (t_ * cos) + (rotate_half(t) * sin)
-#         return torch.cat((t_, t_pass_), dim=-1).type_as(t)
-# =====================然后运行即可成功将模型量化为gptq======================================================================
-# 2、生成safetensors文件，改名为model.safetensors，其他文件都删除。
-# 3、将其他gptq中的qwen文件copy过来。即可运行
-# 4、set CUDA_VISIBLE_DEVICES=0,1,3,2
-# 5、set PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32
-# 6、conda activate qwen
-# 7、python convert_gptq.py
+# Copyright 2025 ModelCloud
+# Contact: qubitium@modelcloud.ai, x.com/qubitium
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import torch
-device=torch.device("cpu")
+from gptqmodel import GPTQModel, QuantizeConfig
+from transformers import AutoTokenizer
 
-logging.basicConfig(
-    format="%(asctime)s %(levelname)s [%(name)s] %(message)s", level=logging.INFO, datefmt="%Y-%m-%d %H:%M:%S"
-)
+original_model_path = '/home/tutu/models/QwQ-32B'
+pretrained_model_id = original_model_path
+quantized_model_id = original_model_path + "-gptq"
 
-pretrained_model_dir = "D:/models/Qwen-72B-Chat"
-# pretrained_model_dir = "facebook/opt-125m"
-quantized_model_dir = "D:/models/Qwen-72B-Chat-gptq"
-# quantized_model_dir = "opt-125m-4bit"
 
-tokenizer = AutoTokenizer.from_pretrained(pretrained_model_dir, use_fast=True, trust_remote_code=True)
-examples = [
-    tokenizer(
-        "auto-gptq is an easy-to-use model quantization library with user-friendly apis, based on GPTQ algorithm."
+def main():
+    print('开始量化...')
+    print('读取tokenizer...')
+
+    tokenizer = AutoTokenizer.from_pretrained(pretrained_model_id, use_fast=True)
+    examples = [
+        tokenizer(
+            "gptqmodel is an easy-to-use model quantization library with user-friendly apis, based on GPTQ algorithm."
+        )
+    ]
+
+    quantize_config = QuantizeConfig(
+        bits=4,  # quantize model to 4-bit
+        group_size=128,  # it is recommended to set the value to 128
     )
-]
 
-quantize_config = BaseQuantizeConfig(
-    bits=4,  # quantize model to 4-bit
-    group_size=128,  # it is recommended to set the value to 128
-    desc_act=False,  # set to False can significantly speed up inference but the perplexity may slightly bad
-)
+    print('读取模型...')
 
-# load un-quantized model, by default, the model will always be loaded into CPU memory
-model = AutoGPTQForCausalLM.from_pretrained(
-    pretrained_model_dir,
-    quantize_config,
-    low_cpu_mem_usage=True,
-    trust_remote_code=True,
-    max_memory={ 0: '22GiB', 1: '22GiB', 2: '22GiB', 3: '22GiB', 'cpu': '55GiB'},
-    # max_memory={ 0: '22GiB', 1: '22GiB', 2: '22GiB', 3: '22GiB', 'cpu': '55GiB', 'disk':'100GiB'},
-    device_map='auto'
-)
+    model = GPTQModel.load(
+        pretrained_model_id,
+        quantize_config=quantize_config,
+    )
 
-# quantize model, the examples should be list of dict whose keys can only be "input_ids" and "attention_mask"
-model.quantize(
-    examples,
-    use_triton=False,
-    batch_size=1,
-    cache_examples_on_gpu=False,
-)
+    print('正在量化...')
 
-# save quantized model
-model.save_quantized(quantized_model_dir)
+    model.quantize(examples)
 
-# save quantized model using safetensors
-model.save_quantized(quantized_model_dir, use_safetensors=True)
+    print('存储量化...')
 
-# push quantized model to Hugging Face Hub.
-# to use use_auth_token=True, Login first via huggingface-cli login.
-# or pass explcit token with: use_auth_token="hf_xxxxxxx"
-# (uncomment the following three lines to enable this feature)
-# repo_id = f"YourUserName/{quantized_model_dir}"
-# commit_message = f"AutoGPTQ model for {pretrained_model_dir}: {quantize_config.bits}bits, gr{quantize_config.group_size}, desc_act={quantize_config.desc_act}"
-# model.push_to_hub(repo_id, commit_message=commit_message, use_auth_token=True)
+    model.save(quantized_model_id)
 
-# alternatively you can save and push at the same time
-# (uncomment the following three lines to enable this feature)
-# repo_id = f"YourUserName/{quantized_model_dir}"
-# commit_message = f"AutoGPTQ model for {pretrained_model_dir}: {quantize_config.bits}bits, gr{quantize_config.group_size}, desc_act={quantize_config.desc_act}"
-# model.push_to_hub(repo_id, save_dir=quantized_model_dir, use_safetensors=True, commit_message=commit_message, use_auth_token=True)
+    tokenizer.save_pretrained(quantized_model_id)
 
+    print('量化完成.')
 
-
-
-# load quantized model to the first GPU
-# model = AutoGPTQForCausalLM.from_quantized(quantized_model_dir, device="cuda:0")
-
-# download quantized model from Hugging Face Hub and load to the first GPU
-# model = AutoGPTQForCausalLM.from_quantized(repo_id, device="cuda:0", use_safetensors=True, use_triton=False)
-
-# inference with model.generate
-# print(tokenizer.decode(model.generate(**tokenizer("auto_gptq is", return_tensors="pt").to(model.device))[0]))
-
-# or you can also use pipeline
-# pipeline = TextGenerationPipeline(model=model, tokenizer=tokenizer)
-# print(pipeline("auto-gptq is")[0]["generated_text"])cls
+if __name__ == "__main__":
+    main()
