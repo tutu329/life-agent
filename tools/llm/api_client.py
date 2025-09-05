bd629d5e39b169209d1f94c10d2896b50fdaf7ef
tutu <jack.seaver@163.com>
2025-09-05 11:15:18 +0800

    更改

--- tools/llm/api_client.py
+++ tools/llm/api_client.py
@@ -1743,12 +1743,12 @@ def reasoning_effort_main():
 def async_reasoning_effort_main():
     from console import print_color
     print_color()
-    # llm_config = llm_protocol.g_local_qwen3_30b_thinking
+    llm_config = llm_protocol.g_local_qwen3_30b_thinking
     # llm_config = llm_protocol.g_local_qwen3_30b_chat
     # llm_config = llm_protocol.g_online_deepseek_chat
     # llm_config = llm_protocol.g_local_qwen3_4b_thinking
     # llm_config = llm_protocol.g_online_groq_gpt_oss_20b
-    llm_config = llm_protocol.g_online_groq_gpt_oss_120b
+    # llm_config = llm_protocol.g_online_groq_gpt_oss_120b
     # llm_config = llm_protocol.g_online_groq_kimi_k2
     # llm_config = llm_protocol.g_local_gpt_oss_20b_mxfp4
     # llm_config.reasoning_effort = LLM_Reasoning_Effort.HIGH
