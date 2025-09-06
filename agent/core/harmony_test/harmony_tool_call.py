afb3111c62073f8a6f51e772379da44e920c8944
tutu <jack.seaver@163.com>
2025-09-07 07:18:36 +0800

    更改

--- agent/core/harmony_test/harmony_tool_call.py
+++ agent/core/harmony_test/harmony_tool_call.py
@@ -485,6 +485,7 @@ def tool_call_agent(last_tool_result=None):
         # reasoning={"effort": 'medium'},
         reasoning={"effort": 'low'},
     )
+
     # ----------------------------response格式构建history-------------------------------
 
     # return await self._client.responses.create(
