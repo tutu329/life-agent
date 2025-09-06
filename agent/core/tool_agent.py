d2b0a976507985d2b7aec4c70c2af519f3d6d7f7
tutu <jack.seaver@163.com>
2025-09-06 13:10:23 +0800

    更改

--- agent/core/tool_agent.py
+++ agent/core/tool_agent.py
@@ -919,8 +919,8 @@ def main_folder():
         tool_names=['Folder_Tool'],
         # llm_config=llm_protocol.g_online_groq_kimi_k2,
         # llm_config=llm_protocol.g_online_groq_gpt_oss_20b,
-        # llm_config=llm_protocol.g_online_groq_gpt_oss_120b,
-        llm_config=llm_protocol.g_local_qwen3_30b_chat,
+        llm_config=llm_protocol.g_online_groq_gpt_oss_120b,
+        # llm_config=llm_protocol.g_local_qwen3_30b_chat,
         has_history=True,
     )
 
