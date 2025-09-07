5fa4c2f432f3e4b960ca23e476e11761ae0af2ed
tutu <jack.seaver@163.com>
2025-09-07 15:53:59 +0800

    更改

--- agent/core/harmony_response.py
+++ agent/core/harmony_response.py
@@ -1,9 +1,22 @@
 from openai import OpenAI, APIError
-from openai.types.responses import ResponseReasoningItem, ResponseFunctionToolCall, ResponseOutputMessage
+from openai.types.responses import Response, ResponseReasoningItem, ResponseFunctionToolCall, ResponseOutputMessage
 from openai.types.responses import ToolParam, FunctionToolParam
 
 from typing import List, Dict, Any, Type, Literal, Optional
 from pydantic import BaseModel, Field, ConfigDict
+
+from pprint import pprint
+
+DEBUG = True
+# DEBUG = False
+
+def dprint(*args, **kwargs):
+    if DEBUG:
+        print(*args, **kwargs)
+
+def dpprint(*args, **kwargs):
+    if DEBUG:
+        pprint(*args, **kwargs)
 
 # ---------------------------------pydantic导出示例---------------------------------
 # class M(BaseModel):
@@ -129,14 +142,62 @@ class Response_Request(BaseModel):
     max_output_tokens   :int = 8192
     reasoning       :Dict = Field(default_factory=lambda: {"effort": 'low'})
 
-class Harmony_Response:
-    def __init__(self, api: OpenAI):
-        pass
+class Response_Result(BaseModel):
+    reasoning   :str = ''
+    output      :str = ''
+    function_tool_call   :ResponseFunctionToolCall = None # {'tool_name':'...', 'tool_args','...'}
+    other_item  :Any = None
 
-    def create(self):
-        pass
+class Response_LLM_Client:
+    def __init__(self, client: OpenAI):
+        self.client = client
 
-def main():
+    def responses_create(self, request:Response_Request):
+        res = self.client.responses.create(**request.model_dump(exclude_none=True))
+        return res
+
+    def responses_result(self, res:Response):
+        # dprint(res)
+        dprint('------------------------------Response---------------------------------------')
+        dpprint(res.model_dump())
+        dprint('-----------------------------/Response---------------------------------------')
+
+        response_result = Response_Result()
+
+        dprint()
+        dprint('==========================================Response Items==========================================')
+        if res.output:
+            for item in res.output:
+                if isinstance(item, ResponseFunctionToolCall):
+                    dprint('---------------------------ResponseFunctionToolCall------------------------------------')
+                    dprint(item)
+                    dprint('--------------------------/ResponseFunctionToolCall------------------------------------')
+                    dprint()
+                    response_result.function_tool_call = item
+                elif isinstance(item, ResponseReasoningItem):
+                    dprint('-----------------------------ResponseReasoningItem-------------------------------------')
+                    dprint(item)
+                    dprint('----------------------------/ResponseReasoningItem-------------------------------------')
+                    dprint()
+                    if item.content and 'text' in item.content[0]:
+                        response_result.reasoning = item.content[0]['text']
+                elif isinstance(item, ResponseOutputMessage):
+                    dprint('-----------------------------ResponseOutputMessage-------------------------------------')
+                    dprint(item)
+                    dprint('----------------------------/ResponseOutputMessage-------------------------------------')
+                    dprint()
+                    if item.content and 'text' in item.content[0]:
+                        response_result.output = item.content[0]['text']
+                else:
+                    dprint('----------------------------------other item-------------------------------------------')
+                    dprint(item)
+                    dprint('---------------------------------/other item-------------------------------------------')
+                    dprint()
+                    response_result.other_item = item
+        dprint('=========================================/Response Items==========================================')
+        return response_result
+
+def main_response_request_pprint():
     input = '你是谁？'
     add_tool = Tool_Request(
         name='add_tool',
@@ -159,8 +220,89 @@ def main():
         tools=tools,
     )
 
-    from pprint import pprint
-    pprint(response_request.model_dump())
+    dpprint(response_request.model_dump())
+
+def main_response_llm_client():
+    add_tool = Tool_Request(
+        name='add_tool',
+        description='加法计算工具',
+        parameters=Tool_Parameters(
+            properties={
+                'a': Tool_Property(type='number', description='加数'),
+                'b': Tool_Property(type='number', description='被加数'),
+                'unit': Tool_Property(type='string', description='单位', enum=['meter', 'kilo-miter']),
+            },
+            required=['a', 'b'],
+        ),
+    )
+    sub_tool = Tool_Request(
+        name='sub_tool',
+        description='减法计算工具',
+        parameters=Tool_Parameters(
+            properties={
+                'a': Tool_Property(type='number', description='减数'),
+                'b': Tool_Property(type='number', description='被减数'),
+                'unit': Tool_Property(type='string', description='单位', enum=['meter', 'kilo-miter']),
+            },
+            required=['a', 'b'],
+        ),
+    )
+    mul_tool = Tool_Request(
+        name='mul_tool',
+        description='乘法计算工具',
+        parameters=Tool_Parameters(
+            properties={
+                'a': Tool_Property(type='number', description='乘数'),
+                'b': Tool_Property(type='number', description='被乘数'),
+                'unit': Tool_Property(type='string', description='单位', enum=['meter', 'kilo-miter']),
+            },
+            required=['a', 'b'],
+        ),
+    )
+    div_tool = Tool_Request(
+        name='div_tool',
+        description='除法计算工具',
+        parameters=Tool_Parameters(
+            properties={
+                'a': Tool_Property(type='number', description='除数'),
+                'b': Tool_Property(type='number', description='被除数'),
+                'unit': Tool_Property(type='string', description='单位', enum=['meter', 'kilo-miter']),
+            },
+            required=['a', 'b'],
+        ),
+    )
+    # tools = []
+    # tools = [div_tool]
+    tools = [add_tool, sub_tool, mul_tool, div_tool]
+
+    input = '请告诉我2356/3567+22*33+3567/8769+4356/5678等于多少，保留10位小数，要调用工具计算，不能直接心算'
+    # input = '你是谁？'
+    response_request = Response_Request(
+        model='openai/gpt-oss-20b',
+        input=input,
+        tools=tools,
+    )
+
+    # -------------打印输入参数--------------
+    # dpprint(response_request.model_dump())
+
+    import httpx, os, json
+    http_client = httpx.Client(proxy="http://127.0.0.1:7890")
+    client = OpenAI(
+        api_key=os.getenv("GROQ_API_KEY") or 'empty',
+        base_url='https://api.groq.com/openai/v1',
+        http_client=http_client,
+    )
+    client = Response_LLM_Client(client=client)
+    res = client.responses_create(request=response_request)
+    responses_result = client.responses_result(res=res)
+
+    dprint()
+    dprint('---------------------------------response result------------------------------------')
+    dpprint(responses_result.model_dump())
+    dprint('--------------------------------/response result------------------------------------')
+
 
 if __name__ == "__main__":
-    main()
+    # main_response_request_pprint()
+    main_response_llm_client()
