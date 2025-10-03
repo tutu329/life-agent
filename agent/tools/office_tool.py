a1c5c9d72cea55884d9ba48c68edfb86698eaf10
tutu <jack.seaver@163.com>
2025-10-03 08:07:46 +0800

    更改

--- agent/tools/office_tool.py
+++ agent/tools/office_tool.py
@@ -1272,9 +1272,9 @@ class Write_Chapter_Tool(Base_Tool):
                 cls._class_call_collabora_api(top_agent_id=top_agent_id, cmd='set_paragraph', params=params)
 
                 # 选择llm和参数
-                # llm_config = config.g_online_groq_kimi_k2
-                # llm_config = config.g_online_deepseek_chat
-                llm_config = llm_protocol.g_local_gpt_oss_120b_mxfp4_lmstudio
+                # llm_config = llm_protocol.g_online_groq_kimi_k2
+                llm_config = llm_protocol.g_online_deepseek_chat
+                # llm_config = llm_protocol.g_local_gpt_oss_120b_mxfp4_lmstudio
                 # llm_config = llm_protocol.g_online_groq_gpt_oss_120b
                 # llm_config = llm_protocol.g_online_deepseek_chat
                 llm = LLM_Client(llm_config=llm_config)
@@ -1454,9 +1454,9 @@ class Write_Chapter_Tool(Base_Tool):
                 self._call_collabora_api(top_agent_id=top_agent_id, cmd='set_paragraph', params=params)
 
                 # 选择llm和参数
-                # llm_config = config.g_online_groq_kimi_k2
-                # llm_config = config.g_online_deepseek_chat
-                llm_config = llm_protocol.g_online_groq_gpt_oss_120b
+                # llm_config = llm_protocol.g_online_groq_kimi_k2
+                llm_config = llm_protocol.g_online_deepseek_chat
+                # llm_config = llm_protocol.g_online_groq_gpt_oss_120b
                 # llm_config = llm_protocol.g_online_deepseek_chat
                 llm = LLM_Client(llm_config=llm_config)
 
