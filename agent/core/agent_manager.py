b8d121fd4bdf22943a41c83475926012c6761a93
tutu <jack.seaver@163.com>
2025-10-03 11:24:41 +0800

    更改

--- agent/core/agent_manager.py
+++ agent/core/agent_manager.py
@@ -65,9 +65,9 @@ class Agent_Manager:
                 # -------------/获取所有的普通local tools--------------
 
                 # -----------获取所有的local agent as tools-----------
-                # for agent in agents:
-                #     if agent.name in cls.allowed_local_tool_names:
-                #         allowd_local_tool_requests.append(agent.as_tool_request)
+                # for agent_as_tool in cls.get_all_agents_as_tool():
+                #     if agent_as_tool['name'] in cls.allowed_local_tool_names:
+                #         allowd_local_tool_requests.append(agent_as_tool.tool_request)
                 # ----------/获取所有的local agent as tools-----------
 
             # 根据MCP url，添加allowed对应的tools
