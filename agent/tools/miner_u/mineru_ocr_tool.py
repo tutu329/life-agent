5d5dfa52745603543a5fcadb183c49fd2d6a8753
tutu <jack.seaver@163.com>
2025-10-08 09:15:29 +0800

    更改

--- agent/tools/miner_u/mineru_ocr_tool.py
+++ agent/tools/miner_u/mineru_ocr_tool.py
@@ -4,7 +4,7 @@
 # conda activate mineru
 # uv pip install mineru[all]
 #
-# 2、使用ui(目前能启动，但ui解析pdf开始后，但用vllm-engine时engine会报错)
+# 2、使用ui(注意要用transformer不要用pipe。)
 # https://opendatalab.github.io/MinerU/usage/quick_usage/#quick-usage-via-command-line
 # 如使用含vllm的ui（启动后，会下载模型，由于开VPN会造成启动测试失败，因此必须关VPN）：
 # export MINERU_MODEL_SOURCE=modelscope
@@ -12,7 +12,9 @@
 # mineru-gradio --server-name 0.0.0.0 --server-port 7860 --enable-vllm-engine true
 # 然后浏览器：http://powerai.cc:7860
 #
-# 3、使用cli(解析当前目录下所有文件如pdf)
+# 3、使用cli(解析当前目录下所有文件如pdf)(--backend [pipeline|vlm-transformers|vlm-vllm-engine|vlm-http-client])
 # export MINERU_MODEL_SOURCE=modelscope
-# mineru -p ./ -o ./
+# mineru -p ./ -o ./ --backend vlm-transformers
+#
+# 4、目前vllm-10.2下，2080ti用vllm-engine时engine会报错。
 
