7ef3290f44f3a22d25abd80599c4728770831b8e
tutu <jack.seaver@163.com>
2025-10-14 11:39:17 +0800

    更改

--- agent/api/client_test.py
+++ agent/api/client_test.py
@@ -1,10 +1,6 @@
-# client_minimal.py
-import requests
-from pprint import pprint
-
+import requests, time
 from agent.core.agent_config import Agent_Config
 import llm_protocol
-import time
 
 BASE_URL = "http://powerai.cc:8005"
 
@@ -22,6 +18,7 @@ def main():
     #     max_new_tokens=8192,
     #     stream=True,
     # )
+    # ------------------------------ 1.1、底层agent as tool ------------------------------
     agent_config = Agent_Config(
         llm_config=llm_c,
         agent_name='agent level 2-Folder_Tool_Level_2',
@@ -31,20 +28,29 @@ def main():
     )
     r = requests.post(f"{BASE_URL}/agents/create_agent", json=agent_config.model_dump(exclude_none=True), timeout=60)
     r.raise_for_status()
-
-
-    # ------------------------------ 2、run_agent(agent_id) ------------------------------
     agent_id = r.json()["agent_id"]
+    print(f'sub-agent as tool(agent_id: {agent_id!r}) created.')
+
+    # ------------------------------ 1.2、上层agent(可以叠加若干层) ------------------------------
+    agent_config = Agent_Config(
+        llm_config=llm_c,
+        agent_name='agent level 1-Folder_Tool_Level_1',
+        allowed_local_tool_names=['Folder_Tool_Level_2'],   # 关键参数，通过'Folder_Tool_Level_2'这个字符串，指定1.1注册的agent作为tool
+    )
+    r = requests.post(f"{BASE_URL}/agents/create_agent", json=agent_config.model_dump(exclude_none=True), timeout=60)
+    r.raise_for_status()
+    agent_id = r.json()["agent_id"]
+    print(f'upper-agent(agent_id: {agent_id!r}) created.')
+
+    # ------------------------------ 2、run_agent(agent_id) 运行上层agent------------------------------
     query = '请告诉我/home/tutu/demo下的哪个子目录里有file_to_find.txt这个文件，需要遍历每一个子文件夹，一定能找到'
     r = requests.post(f"{BASE_URL}/agents/run_agent", params={'agent_id':agent_id, 'query':query}, timeout=60)
-    # print(f'agents.run()结果: {r.json()}')
 
     while True:
         # ------------------------------ 3、get_agent_status(agent_id) ------------------------------
-        r = requests.post(f"{BASE_URL}/agents/get_agent_status", params={'agent_id': agent_id}, timeout=60)
-        res = r.json()
+        res = requests.post(f"{BASE_URL}/agents/get_agent_status", params={'agent_id': agent_id}, timeout=60).json()
         if res.get('result_content') and res['result_content'].get('final_answer') and res['result_content']['final_answer']:
-            print(f"agents.run()结果: {res['result_content']['final_answer']}")
+            print(f"upper-agent(agent_id={agent_id!r})返回结果: {res['result_content']['final_answer']!r}")
             break
 
         time.sleep(1)
