b4ae1730d669b9a2772c8b6a913cfd7e87594fa8
tutu <jack.seaver@163.com>
2025-10-18 17:29:08 +0800

    更改

--- tools/llm/response_and_chatml_api_client.py
+++ tools/llm/response_and_chatml_api_client.py
@@ -633,6 +633,7 @@ class Response_and_Chatml_LLM_Client:
         # print(f'self.llm_id: {self.llm_id}')
         # print(f'chunk: {chunk}')
         # print('---------------/_callback_output--------------------')
+
         if self.extra_agent_info:
             # 作为agent，发送回调信息
             try:
